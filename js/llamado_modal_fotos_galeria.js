9ba58a2b7e419bdd222fc086d873f6c188303cb4
Katzerpa <KZ57221@SupervielleSeguros.AD>
2023-04-23 23:47:32 -0300

    revision e identacion de los files

--- js/llamado_modal_fotos_galeria.js
+++ js/llamado_modal_fotos_galeria.js
@@ -1,29 +1,25 @@
-function loadModalImage( isthis){
+function loadModalImage(isthis) {
   // Get the modal
-//alert(isthis);
-var modal = document.getElementById("myModal");
+  var modal = document.getElementById("myModal");
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById(isthis);
-var modalImg = document.getElementById("content-images");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  var img = document.getElementById(isthis);
+  var modalImg = document.getElementById("content-images");
+  var captionText = document.getElementById("caption");
+  img.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = "none";
+  }
 }
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  modal.style.display = "none";
-}
-}
-
-function initModal(){
-  
-}
 
 
